7710f0eaa9f4c7fa09e31f7e29076f2bed424f40
Radzhabov Rustam <rrradzhabov@edu.hse.ru>
2024-06-01 21:18:18 +0300

    Add display of discipline names in left side of the site as a list.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './css/App.css';
 import SearchAndDeleteTab from './SearchAndDeleteTab'
 import {useState, useEffect} from 'react';
+import { AiOutlineArrowDown } from "react-icons/ai";
 
 function App() {
   const [programs, setPrograms] = useState(false);
@@ -11,16 +12,43 @@ function App() {
         return response.text();
       })
       .then(data => {
-        setPrograms(data);
+        // let arr;
+        // arr = JSON.stringify(data);
+        // arr = arr.substring(2, arr.length - 2);
+        // arr = arr.split(",");
+        // let new_arr = [];
+        // for(let i = 0; i < arr.length; i++) {
+        //   if (i % 2) {
+        //     new_arr.push(JSON.parse("\"" + arr[i] + "\""));
+        //   }
+        // } 
+        // arr = new_arr;
+        // for(let i = 0; i < arr.length; i++) {
+        //   arr[i] = arr[i].substring(arr[i].lastIndexOf(":") + 2, arr[i].length - 2);
+        // }
+        let arr = JSON.stringify(data).slice(2, -2).split(",").filter((_, i) => i % 2).map(el => {
+          let parsed = JSON.parse("\"" + el + "\"");
+          return parsed.slice(parsed.lastIndexOf(":") + 2, -2);
+        });        
+
+        setPrograms(arr.map((element, i) => (
+          <ul key={i} style = {{listStyle: "None", paddingLeft: "0"}}>
+           <button style={{backgroundColor: "transparent", backgroundRepeat: "no-repeat", border: "none", cursor: "pointer", overflow: "hidden", outline: "none"}}><AiOutlineArrowDown /> </button> {element}   
+          </ul>
+        )));
+        //setPrograms(arr);
       });
   }
-  getAllPrograms();
+  useEffect(() => {
+    getAllPrograms();
+  }, []);
   return (
     <div className='App' stlye={{height: "100vh"}}>
       <div id="topPanel">  Educational programs </div>
       <SearchAndDeleteTab />
-      <br/>
-      {programs}
+      <ul style = {{listStyle: "None", paddingLeft: "0"}}>
+        {programs}
+      </ul>
     </div>
   );
 }
